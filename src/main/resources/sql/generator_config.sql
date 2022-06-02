320af76e1e953532337908c00d515212730f5953
陈应姗 <1637320276@qq.com>
2022-06-03 01:31:55 +0800

    优化部分代码

--- src/main/resources/sql/generator_config.sql
+++ src/main/resources/sql/generator_config.sql
@@ -3,15 +3,15 @@
 
  Source Server         : 本地
  Source Server Type    : MySQL
- Source Server Version : 50725
- Source Host           : 127.0.0.1:3306
+ Source Server Version : 50736
+ Source Host           : localhost:3306
  Source Schema         : test
 
  Target Server Type    : MySQL
- Target Server Version : 50725
+ Target Server Version : 50736
  File Encoding         : 65001
 
- Date: 19/03/2021 15:57:57
+ Date: 03/06/2022 01:31:12
 */
 
 SET NAMES utf8mb4;
@@ -34,11 +34,91 @@ CREATE TABLE `generator_config`  (
   `is_trim` char(1) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL COMMENT '是否去除前缀 1是 0否',
   `trim_value` varchar(10) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL COMMENT '前缀内容',
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '代码生成配置表' ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci COMMENT = '代码生成配置表' ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of generator_config
 -- ----------------------------
-INSERT INTO `generator_config` VALUES (1, 'yarns', 'com.yarns.december', 'entity', 'mapper', 'mapper', 'service', 'service.impl', 'controller', '0', 't_');
+INSERT INTO `generator_config` VALUES (1, 'Yarns', 'com.yarns.december', 'entity', 'mapper', 'mapper', 'service', 'service.impl', 'controller', '1', 't_');
+
+-- ----------------------------
+-- Table structure for t_sys_resource
+-- ----------------------------
+DROP TABLE IF EXISTS `t_sys_resource`;
+CREATE TABLE `t_sys_resource`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `resource_name` varchar(70) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '资源名称',
+  `resource_url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '资源url',
+  `resource_type` int(1) NULL DEFAULT NULL COMMENT '资源类型（1菜单 2按钮）',
+  `resource_tag` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '权限标识',
+  `order_no` int(11) NULL DEFAULT NULL COMMENT '排序',
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  `modify_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_sys_resource
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for t_sys_role
+-- ----------------------------
+DROP TABLE IF EXISTS `t_sys_role`;
+CREATE TABLE `t_sys_role`  (
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `role_name` varchar(60) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '角色名称',
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  `modify_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_sys_role
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for t_sys_role_resource
+-- ----------------------------
+DROP TABLE IF EXISTS `t_sys_role_resource`;
+CREATE TABLE `t_sys_role_resource`  (
+  `role_id` int(11) NOT NULL,
+  `resource_id` int(11) NOT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_sys_role_resource
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for t_sys_user
+-- ----------------------------
+DROP TABLE IF EXISTS `t_sys_user`;
+CREATE TABLE `t_sys_user`  (
+  `id` int(20) NOT NULL AUTO_INCREMENT COMMENT 'id',
+  `username` varchar(50) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '账号',
+  `password` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '密码',
+  `nickname` varchar(100) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NULL DEFAULT NULL COMMENT '昵称',
+  `create_time` datetime(0) NULL DEFAULT NULL,
+  `modify_time` datetime(0) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_sys_user
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for t_sys_user_role
+-- ----------------------------
+DROP TABLE IF EXISTS `t_sys_user_role`;
+CREATE TABLE `t_sys_user_role`  (
+  `user_id` int(11) NOT NULL,
+  `role_id` int(11) NOT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_sys_user_role
+-- ----------------------------
 
 SET FOREIGN_KEY_CHECKS = 1;
