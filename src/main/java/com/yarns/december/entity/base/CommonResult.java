9230bff5f1422f0c67376e29acca3f8b8edf3e26
陈应姗 <1637320276@qq.com>
2022-06-03 01:29:53 +0800

    添加返回结构体 优化部分问题代码

---
+++ src/main/java/com/yarns/december/entity/base/CommonResult.java
@@ -0,0 +1,47 @@
+package com.yarns.december.entity.base;
+
+import java.io.Serial;
+import java.io.Serializable;
+
+/**
+ * 如果需要把返回值写入结果 就使用该结构体返回
+ * @author Yarns
+ * @date 2022/6/3
+ */
+@SuppressWarnings({"rawtypes", "unchecked"})
+public class CommonResult<T> extends BaseResult implements Serializable {
+    @Serial
+    private static final long serialVersionUID = -7268040542410707954L;
+    public CommonResult() {
+
+    }
+    public static CommonResult ok() {
+        return baseCreate(200,"操作成功",true);
+    }
+
+    public static CommonResult fail() {
+        return baseCreate(500,"操作失败",false);
+    }
+
+    public static CommonResult fail(String msg) {
+        return baseCreate(500,msg,false);
+    }
+
+    private static <T> CommonResult<T> baseCreate(Integer code, String msg, boolean success) {
+        CommonResult result = new CommonResult();
+        result.setCode(code);
+        result.setSuccess(success);
+        result.setMessage(msg);
+        result.setTimestamp(String.valueOf(System.currentTimeMillis()));
+        return result;
+    }
+
+    public CommonResult<T> setResult(T data) {
+        this.setData(data);
+        return this;
+    }
+
+    public T getData() {
+        return (T) super.getData();
+    }
+}
