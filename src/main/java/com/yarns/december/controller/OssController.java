8d8a292de991b61e2061661999805b5124f1ef4f
陈应姗 <1637320276@qq.com>
2023-05-07 23:10:31 +0800

    todo 添加云存储接口

---
+++ src/main/java/com/yarns/december/controller/OssController.java
@@ -0,0 +1,18 @@
+package com.yarns.december.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 对象存储
+ * @author Yarns
+ */
+@Validated
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/oss")
+public class OssController {
+
+}
