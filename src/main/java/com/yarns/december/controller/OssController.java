1eb994fc29524abbf8555dddd167aad6167605b9
Pan <iwanttellu@gmail.com>
2023-05-08 18:04:48 +0800

    修改描述

---
+++ src/main/java/com/yarns/december/controller/OssController.java
@@ -0,0 +1,20 @@
+package com.yarns.december.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 对象存储
+ * @author LENOVO
+ */
+@Slf4j
+@Validated
+@RestController
+@RequestMapping("oss")
+@RequiredArgsConstructor
+public class OssController {
+
+}
